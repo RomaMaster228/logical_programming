9c806cb3732f0171bd87bfcb9a46cff2f70029e4
RomaMaster228 <81747873+RomaMaster228@users.noreply.github.com>
2021-10-17 20:33:47 +0300

    Done

--- task2.pl
+++ task2.pl
@@ -1,38 +1,21 @@
 % Task 2: Relational Data
 %Вариант 1
 
-%-генерирует список List, по заданному предмету Subject
-list_of_not_passed_student(Subject, List):-
-	bagof(Student, not_passed_subject(Subject, Student), List).
+% Получить таблицу групп и средний балл по каждой из групп
+sum_list([], 0, C) :- C is 0.
+sum_list([H|T], Sum, N) :- sum_list(T, Rest, C), Sum is H + Rest, N is C+1.
+avg_mark([H|_], ANS) :- findall(Z, grade(H, _, Z), R), sum_list(R, Sum, N), ANS is Sum/N.
+all_marks([], RES, N) :- RES is 0, N is 0.
+all_marks([H|T], RES, N) :- avg_mark([H], ANS), all_marks(T, Y, K), N is K+1, RES is Y+ANS.
+res_in_group(X, L, R) :- bagof(Y, student(X, Y), L), all_marks(L, RES, N), R is RES/N.
 
-%-подсчет кол-ва студентов в группе не сдавших экзамен
-count_of_not_passed_student_in_group(Group, Count):-
-	setof(Student, not_passed(Student, Group), List),
-	length(List, Count).
+% Найти двоечников
+bad_mark(S, N) :- bagof(Z, grade(Z, S, 2), N).
 
-%-предикат который находит полную информацию о студенте, а также его оценки за все предметы
-sub_predict(Student, Subject ,Mark , Group):-
-    grade(Student, Subject ,Mark),
-    student(Group, Student).
-    
-%-средняя оценка в заданной группе (если группа не задана, ведется подсчет общей средней оценки)
-middle_mark(Group, Mark):-
-    student(Group, Student),
-    findall(Mark,sub_predict(Student, _,Mark,Group),  List),
-    sum_list(List, Sum),
-    length(List, Length),
-    Mark is Sum/Length.
-
-%-предикат поиск несдавшего экзамен студента
-not_passed(Student, Group):-
-	student(Group, Student),
-	grade(Student, _ ,Mark),
-	Mark < 3.
-
-%-предикат, который ставит в соответствие предмет со студентом, если тот не сдал предмет
-not_passed_subject(Subject, Student):-
-    grade(Student, Subject, Mark),
-    Mark < 3.
+% Найти количество двоечников в каждой из групп
+count_mark_2_in_group([], C) :- C is 0.
+count_mark_2_in_group([H|T], C) :- grade(H, _, 2) -> count_mark_2_in_group(T, N), C is N+1; count_mark_2_in_group(T, N), C is N.
+count_mark_2(X, C) :- bagof(Y, student(X, Y), L), count_mark_2_in_group(L, C).
 
 % The line below imports the data
 :- ['one.pl'].
